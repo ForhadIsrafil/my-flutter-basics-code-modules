de6a7d8e9d59e190bdcd57257833cc50b76eb163
forhadisrafil <forhadtuhin5088@egmail.com>
2023-09-07 04:01:33 +0600

    appbar

---
+++ lib/AppBarExamples/ConvexAppBar.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:convex_bottom_bar/convex_bottom_bar.dart';
+
+class ConvexAppBar extends StatefulWidget {
+  const ConvexAppBar({super.key});
+
+  @override
+  State<ConvexAppBar> createState() => _ConvexAppBarState();
+}
+
+class _ConvexAppBarState extends State<ConvexAppBar> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
